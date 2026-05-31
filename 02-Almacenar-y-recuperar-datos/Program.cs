1f900d037f3c8fa8c86adbfb349239d3838a52ee
MichaelLunaDev <michael.urbano2008@gmail.com>
2026-05-31 02:03:21 -0500

    Certificado 2 Tipos de datos y variables

---
+++ 02-Almacenar-y-recuperar-datos/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.WriteLine('b');
+// ERROR POR MUCHOS CARACTERES Console.WriteLine('Hello World!');
+Console.WriteLine(123);
+// IMPRIME FLOAT
+Console.WriteLine(0.25F);
+// IMPRIME DOUBLE
+Console.WriteLine(2.625);
+// IMPRIME DECIMAL
+Console.WriteLine(12.39816m);
+// Booleans
+Console.WriteLine(true);
+Console.WriteLine(false);
+// Si no se trabaja con los enteros o booleans mejor usar strings
+// Output: 123
+Console.WriteLine("123");
+// Output: 123
+Console.WriteLine(123);
+
+// Output: true
+Console.WriteLine("true");
+// Output: True
+Console.WriteLine(true);
+
+// DECLARAR VARIABLES
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
+
+string firstName;
+firstName = "Bob";
+
+// ERROR POR POSICION
+//string SecondName; 
+//"Bob" = SecondName;
+
+//string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+//string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+//string firstName;
+Console.WriteLine(firstName);
+
+// VAR
+var message = "Hello world!";
+
+// ERROR
+//var message = "Hello World!";
+//message = 10.703m;
+
+// ERROR
+//var message;
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
